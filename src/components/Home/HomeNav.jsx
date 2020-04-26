9c9651a82850d18b76a677fc19b2735956966170
Lucas Dolsan <lucas.dolsan.1775@gmail.com>
2020-04-26 20:40:20 -0300

    arg de esconder Página inicial

--- src/components/Home/HomeNav.jsx
+++ src/components/Home/HomeNav.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { NavBar } from '../Home/components/NavBar'
-import { Container } from 'react-bootstrap'
-
-
-
+import React from "react";
+import { NavBar } from "../Home/components/NavBar";
+import { Container } from "react-bootstrap";
 
 function HomeNav() {
-
   return (
     <>
-      <NavBar nome={"Cronograma UHSP: "} />
+      <NavBar hideHomeLink={true} nome={"Cronograma UHSP: "} />
       <Container>
-        <h1 className="text-center" style={{ marginTop: "250px" }}>Gerenciador de Cronogramas</h1>
+        <h1 className="text-center" style={{ marginTop: "250px" }}>
+          Gerenciador de Cronogramas
+        </h1>
         <p className="text-center">
-          Uma ótima maneira de organizar seus cronogramas, de maneira rápida e fácil!!!
-    </p>
+          Uma ótima maneira de organizar seus cronogramas, de maneira rápida e
+          fácil!
+        </p>
       </Container>
     </>
-  )
+  );
 }
 
-export default HomeNav
+export default HomeNav;
--- src/components/Home/components/NavBar/NavBar.jsx
+++ src/components/Home/components/NavBar/NavBar.jsx
@@ -1,26 +1,24 @@
-import React, { useState, forwardRef, useImperativeHandle } from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 
-
-function NavBar({ nome }) {
-
-
+function NavBar({ nome, hideHomeLink }) {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand >{nome}</Navbar.Brand>
+      <Navbar.Brand>{nome}</Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="/cronogramas" >Meus Cronogramas</Nav.Link>
+        {!hideHomeLink && <Nav.Link href="/home">Página inicial</Nav.Link>}
+        <Nav.Link href="/cronogramas">Meus Cronogramas</Nav.Link>
         <Nav.Link href="/cadastro">Cadrastar Colaborador</Nav.Link>
       </Nav>
       <Form inline>
         <FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
         <Button variant="outline-light">Buscar</Button>
       </Form>
-      <Nav >
-        <Nav.Link className="justify-content-end tc-light" >Sair</Nav.Link>
+      <Nav>
+        <Nav.Link className="justify-content-end tc-light">Sair</Nav.Link>
       </Nav>
     </Navbar>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
